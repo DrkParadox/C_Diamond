523ed6c38d668b00adcb9cda576f0b6ce6fe829f
DrkParadox <drkparadoxandreidik@protonmail.com>
2016-11-27 02:35:53 +0100

    Update Diamond.c

--- Diamond.c
+++ Diamond.c
@@ -2,3 +2,38 @@
 
 int main (void)
 
+int i,j;
+	int longitud;
+	
+	printf("Give me the rectangle long:");
+	scanf("%i",&longitud);
+
+	for(i=1; i<=longitud; i++ )
+	{
+		printf("* ");	
+	}	
+	printf("\n");
+	
+	i=0;
+	while(i<(longitud/2))
+	{
+		printf("* ");
+		j=0;
+		while(j<longitud-2)
+		{
+			printf("  ");			
+			j++;
+		}
+		printf("*\n");
+		i++;
+	}
+	
+	i=0;
+	do{ 
+		printf("* ");	
+		i++;
+	}while(i!=longitud);		
+	
+	return 0;
+}
+
